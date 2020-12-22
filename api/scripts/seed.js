9d7df48ed875ab472e9c11967922f86d4cf303a0
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2020-12-22 14:48:12 +0100

    Added await in an async function

--- api/scripts/seed.js
+++ api/scripts/seed.js
@@ -2,7 +2,7 @@ const { cryptPassword } = require('../utils/encryption');
 const User = require('../models/User.js');
 
 const createUsers = async () => {
-  return User
+  return await User
     .findOrCreate({
       where: {email: 'john@doe.com'},
       defaults: {password: await cryptPassword('Azerty123')}
--- api/scripts/sync.js
+++ api/scripts/sync.js
@@ -1,8 +1,9 @@
+const { TestScheduler } = require('jest');
 const sequelize = require('../db/db.js');
 const User = require('../models/User.js');
 
 const createTables = async () => {
-  sequelize.sync({ force: true });
+  const result = await sequelize.sync({ force: true });
 }
 
 createTables()
