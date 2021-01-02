8693f48a852fd07fad7ceebe49ffc44ab0c4e4f7
Feruzteame <teamefitwi@gmail.com>
2021-01-02 15:14:27 +0100

    landing-page

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+import './LandingSec.css';
+
+
+function Footer() {
+    return ( 
+        <div className = "footer" >
+            <p><span>&#169;</span>2021 Dream Team All Right Reserved</p>
+        </div>
+    );
+}
+
+export default Footer
