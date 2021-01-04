66a6b65572594b0722f34fe18dfceb44fbae5c2f
Iryna Spyrydonova <Iryna059@gmail.com>
2021-01-04 15:26:26 +0100

    removed comments

--- client/src/components/pages/MySchools.js
+++ client/src/components/pages/MySchools.js
@@ -9,26 +9,6 @@ const MySchools = props =>{
         <div>
             
              <h1>My school</h1>
-            {/* <ul className="list-group">
-                {
-                    todos.map(todo =>{
-                        return <TodoItem key={todo._id} todo={todo}/>
-                    })
-                }
-            </ul>
-            <br/>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="todo">Enter Todo</label>
-                <input type="text" 
-                       name="todo" 
-                       value={todo.name} 
-                       onChange={onChange}
-                       className="form-control"
-                       placeholder="Please Enter Todo"/>
-                <button className="btn btn-lg btn-primary btn-block" 
-                        type="submit">Submit</button>
-            </form>
-            {message ? <Message message={message}/> : null} */}
         </div>
     );
 
